423dc993258b21510d4e8a5d790ab35576a58983
Chris Black <black@chrisblack.net>
2019-07-24 07:48:44 -0700

    Minor edit in comment.

--- application/model/model.php
+++ application/model/model.php
@@ -107,7 +107,7 @@ class Model
         // Get those apostrophes and quotes escaped.
         $data = addslashes($data);
 
-        // Start parsing.
+        // Start parsing the data, which has been copied from an Excel file and pasted into a textarea.
         $fish = str_getcsv($data, "\n"); 
         foreach($fish as $account) {
             $account = str_getcsv($account, ";");   // $account = each row         
@@ -149,36 +149,36 @@ class Model
                                               `crnt_rntdesc`, 
                                               `crnt_extra1`, 
                                               `crnt_extra2`) 
-                          VALUES ('$item[0]',
-                                  '$item[1]',
-                                  '$item[2]',
-                                  '$item[3]',
-                                  '$item[4]',
-                                  '$item[5]',
-                                  '$item[6]',
-                                  '$item[7]',
-                                  '$item[8]',
-                                  '$item[9]',
-                                  '$item[10]',
-                                  '$item[11]',
-                                  '$item[12]',
-                                  '$item[13]',
-                                  '$item[14]',
-                                  '$item[15]',
-                                  '$item[16]',
-                                  '$item[17]',
-                                  '$item[18]',
-                                  '$item[19]',
-                                  '$item[20]',
-                                  '$item[21]',
-                                  '$item[22]',
-                                  '$item[23]',
-                                  '$item[24]',
-                                  '$item[25]',
-                                  '$item[26]',
-                                  '$item[27]',
-                                  '$item[28]',
-                                  '$item[29]')
+                                      VALUES ('$item[0]',
+                                              '$item[1]',
+                                              '$item[2]',
+                                              '$item[3]',
+                                              '$item[4]',
+                                              '$item[5]',
+                                              '$item[6]',
+                                              '$item[7]',
+                                              '$item[8]',
+                                              '$item[9]',
+                                              '$item[10]',
+                                              '$item[11]',
+                                              '$item[12]',
+                                              '$item[13]',
+                                              '$item[14]',
+                                              '$item[15]',
+                                              '$item[16]',
+                                              '$item[17]',
+                                              '$item[18]',
+                                              '$item[19]',
+                                              '$item[20]',
+                                              '$item[21]',
+                                              '$item[22]',
+                                              '$item[23]',
+                                              '$item[24]',
+                                              '$item[25]',
+                                              '$item[26]',
+                                              '$item[27]',
+                                              '$item[28]',
+                                              '$item[29]')
                   ");
                 $query = $this->db->prepare($sql);
                 if(!$query->execute()) {
